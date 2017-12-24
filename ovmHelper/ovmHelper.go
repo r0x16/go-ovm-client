3f1aeca94f0f1562735cc4286d91f27a8fd3d5be
Björn Ahl <bnal@ba78.me>
2017-12-24 09:38:12 +0100

    work

--- ovmHelper/ovmHelper.go
+++ ovmHelper/ovmHelper.go
@@ -1,6 +1,10 @@
 package ovmHelper
 
 import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
@@ -13,8 +17,21 @@ type Client struct {
 	Vms      *Vm
 }
 
+func NewClient(user string, password string, baseUrl string) *Client {
+	baseURL, _ := url.Parse(baseUrl)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	c := &Client{User: user, Password: password, BaseURL: baseURL}
+	//c.client = http.DefaultClient{Transport: tr}
+	c.client = &http.Client{Transport: tr}
+	c.Vms = &Vm{client: c}
+	return c
+}
+
 func (pc *Client) NewRequest(method string, rsc string, params map[string]string) (*http.Request, error) {
 	baseUrl, err := url.Parse(pc.BaseURL.String() + rsc)
+
 	if err != nil {
 		return nil, err
 	}
@@ -29,6 +46,8 @@ func (pc *Client) NewRequest(method string, rsc string, params map[string]string
 
 	req, err := http.NewRequest(method, baseUrl.String(), nil)
 	req.SetBasicAuth(pc.User, pc.Password)
+	req.Header.Add("content-type", "application/json; charset=utf-8")
+	req.Header.Add("Accept", "application/json")
 	return req, err
 }
 
@@ -39,11 +58,28 @@ func (pc *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	}
 	defer resp.Body.Close()
 
-	if err := validateResponse(resp); err != nil {
+	//body, err := ioutil.ReadAll(resp.Body)
+
+	//fmt.Println(string(body))
+
+	/*if err := validateResponse(resp); err != nil {
 		return resp, err
 	}
 
+
+	*/
 	err = decodeResponse(resp, v)
 	return resp, err
 
+}
+
+func decodeResponse(r *http.Response, v interface{}) error {
+	if v == nil {
+		return fmt.Errorf("nil interface provided to decodeResponse")
+	}
+
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	bodyString := string(bodyBytes)
+	err := json.Unmarshal([]byte(bodyString), &v)
+	return err
 }
--- ovmHelper/vm.go
+++ ovmHelper/vm.go
@@ -4,17 +4,18 @@ type Vm struct {
 	client *Client
 }
 
-func (v *Vm) Read(id string) (*CheckResponse, error) {
-	req, err := cs.client.NewRequest("GET", "/ovm/core/wsapi/rest/Vm/"+id, nil)
+func (v *Vm) Read(id string) (*VmResponse, error) {
+	req, err := v.client.NewRequest("GET", "/ovm/core/wsapi/rest/Vm/"+id, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	m := &vmDetailsJsonResponse{}
-	_, err = vm.client.Do(req, m)
+	m := &VmResponse{}
+	_, err = v.client.Do(req, m)
+
 	if err != nil {
 		return nil, err
 	}
 
-	return m.Check, err
+	return m, err
 }
