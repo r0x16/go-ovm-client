979cca4a3fbdf98e365c06f0d9940578ded130bc
Björn Ahl <bnal@ba78.me>
2017-12-24 09:37:42 +0100

    vm struct

--- ovmHelper/api_responses.go
+++ ovmHelper/api_responses.go
@@ -1,24 +1,50 @@
-package ovmHelpr
+package ovmHelper
 
-type vmDetailsJsonResponse struct {
-	repositoryId int64
-	serverPoolId int64
+type id struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+	Uri   string `json:"uri"`
+	Name  string `json:"name"`
+}
+
+type VmResponse struct {
+	//RepositoryId int64 `json:"repositoryId"`
+	//serverPoolId int64
+	Id   id     `json:"id"`
+	Name string `json:"name"`
 	//The following fields of the new Vm are optional:
-	bootOrder          []string
-	cpuCount           int
-	cpuCountLimit      int
-	cpuPriority        int
-	cpuUtilizationCap  int
-	highAvailability   bool
-	hugePagesEnabled   bool
-	keymapName         sting
-	memory             int
-	memoryLimit        int
-	networkInstallPath string
-	osType             string
-	serverId           int64
-	vmDomainType       string
-	vmMouseType        string
-	vmRunState         string
-	vmStartPolicy      string
+	//BootOrder          []string
+	CpuCount                 int    `json:"cpuCount"`
+	CpuCountLimit            int    `json:"cpuCountLimit"`
+	CpuPriority              int    `json:"cpuPriority"`
+	CpuUtilizationCap        int    `json:"cpuUtilizationCap"`
+	HighAvailability         bool   `json:"highAvailability"`
+	HugePagesEnabled         bool   `json:"hugePagesEnabled"`
+	KeymapName               string `json:"keymapName"`
+	Memory                   int    `json:"memory"`
+	MemoryLimit              int    `json:"memoryLimit"`
+	NetworkInstallPath       string `json:"networkInstallPath"`
+	OsType                   string `json:"osType"`
+	VmDomainType             string `json:"vmDomainType"`
+	VmMouseType              string `json:"vmMouseType"`
+	VmRunState               string `json:"vmRunState"`
+	VmStartPolicy            string `json:"vmStartPolicy"`
+	SslVncPort               int    `json:""sslVncPort"`
+	SslTtyPort               string `json:""sslTtyPort"`
+	AffinityGroupIds         []int  `json:""affinityGroupIds"`
+	OsVersion                string `json:""osVersion"`
+	KernelVersion            string `json:""kernelVersion"`
+	GuestDriverVersion       string `json:""guestDriverVersion"`
+	VmApiVersion             string `json:""vmApiVersion"`
+	CurrentMemory            int    `json:""currentMemory"`
+	CurrentCpuCount          int    `json:""currentCpuCount"`
+	CurrentDomainId          int    `json:""currentDomainId"`
+	VmConfigFileAbsolutePath string `json:""vmConfigFileAbsolutePath"`
+	VmConfigFileMountedPath  string `json:""vmConfigFileMountedPath"`
+	Architecture             string `json:""architecture"`
+	VmDiskMappingIds         []id   `json:"vmDiskMappingIds"`
+	VirtualNicIds            []id   `json:"virtualNicIds"`
+	RepositoryId             id     `json:"repositoryId"`
+	ServerPoolId             id     `json:"serverPoolId"`
+	ServerId                 id     `json:"serverId"`
 }
