0969065a02269646bec9916193b4a99f16c3287e
Björn Ahl <bnal@ba78.me>
2017-12-30 23:54:55 +0100

    Fix Id problem when creating vm

--- ovmHelper/vmTypes.go
+++ ovmHelper/vmTypes.go
@@ -1,7 +1,7 @@
 package ovmHelper
 
 type Vm struct {
-	Id   Id     `json:"id,omitempty"`
+	Id   *Id    `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
 	//The following fields of the new Vm are optional:
 	//BootOrder          []string
@@ -33,35 +33,10 @@ type Vm struct {
 	VmConfigFileAbsolutePath string `json:"vmConfigFileAbsolutePath,omitempty"`
 	VmConfigFileMountedPath  string `json:"vmConfigFileMountedPath,omitempty"`
 	Architecture             string `json:"architecture,omitempty"`
-	VmDiskMappingIds         []Id   `json:"vmDiskMappingIds,omitempty"`
-	VirtualNicIds            []Id   `json:"virtualNicIds,omitempty"`
-	RepositoryId             Id     `json:"repositoryId,omitempty"`
-	ServerPoolId             Id     `json:"serverPoolId,omitempty"`
-	ServerId                 Id     `json:"serverId,omitempty"`
+	VmDiskMappingIds         *[]Id  `json:"vmDiskMappingIds,omitempty"`
+	VirtualNicIds            *[]Id  `json:"virtualNicIds,omitempty"`
+	RepositoryId             *Id    `json:"repositoryId,omitempty"`
+	ServerPoolId             *Id    `json:"serverPoolId,omitempty"`
+	ServerId                 *Id    `json:"serverId,omitempty"`
 	Generation               int    `json:"generation,omitempty"`
 }
-
-/*type Vm struct {
-	Id           *Id `json:"id,omitempty"`
-	RepositoryId Id  `json:"repositoryId,omitempty"`
-	ServerPoolId Id  `json:"serverPoolId,omitempty"`
-	//The following fields of the new Vm are optional:
-	//	BootOrder          []string `json:"bootOrder"`
-	CpuCount           int    `json:"cpuCount,omitempty"`
-	CpuCountLimit      int    `json:"cpuCountLimit,omitempty"`
-	CpuPriority        int    `json:"cpuPriority,omitempty"`
-	CpuUtilizationCap  int    `json:"cpuUtilizationCap,omitempty"`
-	HighAvailability   bool   `json:"highAvailability,omitempty"`
-	HugePagesEnabled   bool   `json:"hugePagesEnabled,omitempty"`
-	KeymapName         string `json:"keymapName,omitempty"`
-	Memory             int    `json:"memory,omitempty"`
-	MemoryLimit        int    `json:"memoryLimit,omitempty"`
-	NetworkInstallPath string `json:"networkInstallPath,omitempty"`
-	OsType             string `json:"osType,omitempty"`
-	ServerId           string `json:"serverId,omitempty"`
-	VmDomainType       string `json:"vmDomainType,omitempty"`
-	VmMouseType        string `json:"vmMouseType,omitempty"`
-	VmRunState         string `json:"vmRunState,omitempty"`
-	VmStartPolicy      string `json:"vmStartPolicy,omitempty"`
-}
-*/
