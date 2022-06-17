ba00ae1284d8c658bb38c48a8a2ac451b407f6a2
r0x16 <sir.thomas.lara@gmail.com>
2022-06-17 11:00:40 -0400

    issue/X001-07 - Struct reference over value

--- ovmHelper/repositoryType.go
+++ ovmHelper/repositoryType.go
@@ -1,19 +1,19 @@
 package ovmHelper
 
 type Repository struct {
-	Id                 Id       `json:"id,omitempty"`
+	Id                 *Id      `json:"id,omitempty"`
 	Name               string   `json:"name,omitempty"`
 	Description        string   `json:"description,omitempty"`
 	Locked             bool     `json:"locked,omitempty"`
 	ReadOnly           bool     `json:"readOnly,omitempty"`
 	Generation         int      `json:"generation,omitempty"`
 	UserData           []string `json:"userData,omitempty"`
-	ResourceGroupIds   []Id     `json:"resourceGroupIds,omitempty"`
-	VirtualDiskIds     []Id     `json:"virtualDiskIds,omitempty"`
-	VMIds              []Id     `json:"vmIds,omitempty"`
-	AssemblyIds        []Id     `json:"assemblyIds,omitempty"`
-	PresentedServerIds []Id     `json:"presentedServerIds,omitempty"`
-	FileSystemID       Id       `json:"fileSystemId,omitempty"`
+	ResourceGroupIds   *[]Id    `json:"resourceGroupIds,omitempty"`
+	VirtualDiskIds     *[]Id    `json:"virtualDiskIds,omitempty"`
+	VMIds              *[]Id    `json:"vmIds,omitempty"`
+	AssemblyIds        *[]Id    `json:"assemblyIds,omitempty"`
+	PresentedServerIds *[]Id    `json:"presentedServerIds,omitempty"`
+	FileSystemID       *Id      `json:"fileSystemId,omitempty"`
 	ManagerUUID        string   `json:"managerUuid,omitempty"`
 	Refreshed          bool     `json:"refreshed,omitempty"`
 	SharePath          string   `json:"sharePath,omitempty"`
--- ovmHelper/saType.go
+++ ovmHelper/saType.go
@@ -1,20 +1,20 @@
 package ovmHelper
 
 type Sa struct {
-	Id                   Id       `json:"id,omitempty"`
+	Id                   *Id      `json:"id,omitempty"`
 	Name                 string   `json:"name,omitempty"`
 	Description          string   `json:"description,omitempty"`
 	Locked               bool     `json:"locked,omitempty"`
 	ReadOnly             bool     `json:"readOnly,omitempty"`
 	Generation           int      `json:"generation,omitempty"`
 	UserData             []string `json:"userData,omitempty"`
-	ResourceGroupIds     []Id     `json:"resourceGroupIds,omitempty"`
-	ZoneIds              []Id     `json:"zoneIds,omitempty"`
-	AccessGroupIds       []Id     `json:"accessGroupIds,omitempty"`
-	StorageArrayPluginID Id       `json:"storageArrayPluginId,omitempty"`
-	AdminServerIds       []Id     `json:"adminServerIds,omitempty"`
-	VolumeGroupIds       []Id     `json:"volumeGroupIds,omitempty"`
-	ServerID             Id       `json:"serverId,omitempty"`
+	ResourceGroupIds     *[]Id    `json:"resourceGroupIds,omitempty"`
+	ZoneIds              *[]Id    `json:"zoneIds,omitempty"`
+	AccessGroupIds       *[]Id    `json:"accessGroupIds,omitempty"`
+	StorageArrayPluginID *Id      `json:"storageArrayPluginId,omitempty"`
+	AdminServerIds       *[]Id    `json:"adminServerIds,omitempty"`
+	VolumeGroupIds       *[]Id    `json:"volumeGroupIds,omitempty"`
+	ServerID             *Id      `json:"serverId,omitempty"`
 	AdminHostname        string   `json:"adminHostname,omitempty"`
 	AdminUsername        string   `json:"adminUsername,omitempty"`
 	AdminPassword        string   `json:"adminPassword,omitempty"`
