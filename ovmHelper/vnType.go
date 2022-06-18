d2bd13d1455936eaad41194fa7ffe83ccb4e81dc
r0x16 <sir.thomas.lara@gmail.com>
2022-06-18 12:06:07 -0400

    issue/X001-08 - VirtualNic Struct Correction

--- ovmHelper/vnType.go
+++ ovmHelper/vnType.go
@@ -1,16 +1,17 @@
 package ovmHelper
 
 type Vn struct {
-	MacAddress string `json:"macAddress,omitempty"`
-	//IpAddresses   *[]Id  `json:"ipAddresses,omitempty"`
-	//InterfaceName string `json:"interfaceName,omitempty"`
-	VmId      *Id    `json:"vmId,omitempty"`
-	NetworkId *Id    `json:"networkId,omitempty"`
-	Id        *Id    `json:"id,omitempty"`
-	Name      string `json:"name,omitempty"`
-	//Description      string `json:description,omitempty"`
-	//Locked           bool   `json:locked,omitempty"`
-	//ReadOnly         bool   `json:readOnly,omitempty"`
-	Generation int `json:"generation,omitempty"`
-	//ResourceGroupIds *[]Id  `json:resourceGroupIds,omitempty"`
+	Id               *Id      `json:"id,omitempty"`
+	Name             string   `json:"name,omitempty"`
+	Description      string   `json:"description,omitempty"`
+	Locked           bool     `json:"locked,omitempty"`
+	ReadOnly         bool     `json:"readOnly,omitempty"`
+	Generation       int      `json:"generation,omitempty"`
+	UserData         *[]Id    `json:"userData,omitempty"`
+	ResourceGroupIds *[]Id    `json:"resourceGroupIds,omitempty"`
+	MacAddress       string   `json:"macAddress,omitempty"`
+	IPAddresses      []string `json:"ipAddresses,omitempty"`
+	InterfaceName    string   `json:"interfaceName,omitempty"`
+	VmId             *Id      `json:"vmId,omitempty"`
+	NetworkId        *Id      `json:"networkId,omitempty"`
 }
